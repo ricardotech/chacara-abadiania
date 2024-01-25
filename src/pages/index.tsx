555fa97c1b21b6c4a4546909c9ed6d5806f6743b
Ricardo Fonseca <ricardofsdomene@icloud.com>
2024-01-24 21:44:46 -0300

    Facebook Pixel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -67,6 +67,55 @@ export default function Home() {
     );
   };
 
+  const FacebookPixel = () => {
+    return (
+      <>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '753642966276372');
+          fbq('track', 'PageView');
+        `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=753642966276372&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </>
+    );
+  };
+
+  const fbpixel = `
+  <script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '753642966276372');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=753642966276372&ev=PageView&noscript=1"
+/></noscript>
+  `;
+
   useEffect(() => {
     import("react-facebook-pixel")
       .then((x) => x.default)
@@ -83,7 +132,9 @@ export default function Home() {
   return (
     <>
       <Head>
+        <script dangerouslySetInnerHTML={{ __html: fbpixel }}></script>
         <title>🌾 Excelente oportunidade!</title>
+
         <meta
           name="description"
           content="Excelente Oportunidade de Chacara À Venda em Abadiania - GO"
