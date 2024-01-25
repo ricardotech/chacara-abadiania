0cbf8363a00d69cf62c4d98e0311980843cdc3ce
Ricardo Fonseca <ricardofsdomene@icloud.com>
2024-01-24 21:31:36 -0300

    Facebook Pixel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -67,6 +67,37 @@ export default function Home() {
     );
   };
 
+  const FacebookPixel = () => {
+    return (
+      <>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '753642966276372');
+            fbq('track', 'PageView');
+          `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=753642966276372&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </>
+    );
+  };
+
   return (
     <>
       <Head>
@@ -78,6 +109,8 @@ export default function Home() {
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+
+        <FacebookPixel />
       </Head>
       <Flex
         flexDir="column"
