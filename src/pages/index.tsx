17705d468328c67f982afb9cd8cbf8c9bd9e7db3
Ricardo Fonseca <ricardofsdomene@icloud.com>
2024-01-26 18:06:55 -0300

    Pixel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -67,39 +67,9 @@ export default function Home() {
     );
   };
 
-  const FacebookPixel = () => {
-    return (
-      <>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '753642966276372');
-          fbq('track', 'PageView');
-        `,
-          }}
-        />
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=753642966276372&ev=PageView&noscript=1"
-          />
-        </noscript>
-      </>
-    );
-  };
-
   const fbpixel = `
-  <script>
+<!-- Meta Pixel Code -->
+<script>
 !function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -108,26 +78,14 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '753642966276372');
+fbq('init', '757900815722061');
 fbq('track', 'PageView');
 </script>
 <noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=753642966276372&ev=PageView&noscript=1"
+src="https://www.facebook.com/tr?id=757900815722061&ev=PageView&noscript=1"
 /></noscript>
+<!-- End Meta Pixel Code -->
   `;
-
-  useEffect(() => {
-    import("react-facebook-pixel")
-      .then((x) => x.default)
-      .then((ReactPixel) => {
-        ReactPixel.init("753642966276372");
-        ReactPixel.pageView();
-
-        router.events.on("routeChangeComplete", () => {
-          ReactPixel.pageView();
-        });
-      });
-  }, [router.events]);
 
   return (
     <>
