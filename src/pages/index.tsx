3b833f66cc9705b138db6c43fc138677dd852e0d
Ricardo Fonseca <ricardofsdomene@icloud.com>
2024-01-26 18:13:29 -0300

    PIXEL

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -68,36 +68,9 @@ export default function Home() {
     );
   };
 
-  useEffect(() => {
-    import("react-facebook-pixel")
-      .then((x) => x.default)
-      .then((ReactPixel) => {
-        ReactPixel.init("757900815722061"); // facebookPixelId
-        ReactPixel.pageView();
-
-        router.events.on("routeChangeComplete", () => {
-          ReactPixel.pageView();
-        });
-      });
-  }, [router.events]);
-
   return (
     <>
       <Head>
-        <Script id="facebook-pixel">
-          {`
-      !function(f,b,e,v,n,t,s)
-      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-      n.queue=[];t=b.createElement(e);t.async=!0;
-      t.src=v;s=b.getElementsByTagName(e)[0];
-      s.parentNode.insertBefore(t,s)}(window, document,'script',
-      'https://connect.facebook.net/en_US/fbevents.js');
-      fbq('init', '757900815722061');
-      fbq('track', 'PageView');
-      `}
-        </Script>
         <title>🌾 Excelente oportunidade!</title>
 
         <meta
