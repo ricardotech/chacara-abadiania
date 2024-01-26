aa6c69bf3eca2992aa75ccfa39db1337043a42dc
Ricardo Fonseca <ricardofsdomene@icloud.com>
2024-01-26 18:10:08 -0300

    FB Pixel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,6 +23,7 @@ import { PiBoot } from "react-icons/pi";
 import { GiPlantRoots } from "react-icons/gi";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import Script from "next/script";
 
 export default function Home() {
   const { dimensions } = useUtils();
@@ -67,30 +68,23 @@ export default function Home() {
     );
   };
 
-  const fbpixel = `
-<!-- Meta Pixel Code -->
-<script>
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '757900815722061');
-fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=757900815722061&ev=PageView&noscript=1"
-/></noscript>
-<!-- End Meta Pixel Code -->
-  `;
-
   return (
     <>
       <Head>
-        <script dangerouslySetInnerHTML={{ __html: fbpixel }}></script>
+        <Script id="facebook-pixel">
+          {`
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '757900815722061');
+      fbq('track', 'PageView');
+      `}
+        </Script>
         <title>🌾 Excelente oportunidade!</title>
 
         <meta
